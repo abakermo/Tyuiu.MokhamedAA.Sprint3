811943648f94ea2cbf2f316e31348b9faef3a5d1
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-04 16:55:14 +0300

    Task2

--- Tyuiu.MokhamedAA.Sprint3.Task2.V12.Lib/DataService.cs
+++ Tyuiu.MokhamedAA.Sprint3.Task2.V12.Lib/DataService.cs
@@ -1,19 +1,18 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
-namespace Tyuiu.MokhamedAA.Sprint3.Task2.V12.Lib;
-
+namespace Tyuiu.MokhamedAA.Sprint3.Task2.V12.Lib
+{
     public class DataService : ISprint3Task2V12
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
-            public double GetMultiplySeries(int value, int startValue, int stopValue)
+            double multSeries = 1;
+            for (int i = startValue; i <= stopValue; i++)
             {
-                double multSeries = 1;
-                for (int i = startValue; i <= stopValue; i++)
-                {
-                    multSeries = multSeries * Math.Pow((300 / (startValue + Math.Pow(value, startValue))), startValue);
-                    startValue++;
-                }
-                return Math.Round(multSeries, 3);
+                multSeries = multSeries * Math.Pow((300 / (startValue + Math.Pow(value, startValue))), startValue);
+                startValue++;
             }
+            return Math.Round(multSeries, 3);
         }
     }
-
+}
 
