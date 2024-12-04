14a985b73089c1b3c5f9b6c756e36191ed554ab5
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-04 16:46:03 +0300

    Task2

---
+++ Tyuiu.MokhamedAA.Sprint3.Task2.V12/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.MokhamedAA.Sprint3.Task2.V12.Lib;
+namespace Tyuiu.MokhamedAA.Sprint3.Task2.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
