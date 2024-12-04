192ff6df4e2d182ef15be884791eacdad22178c3
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-04 17:29:56 +0300

    Task5

---
+++ Tyuiu.MokhamedAA.Sprint3.Task5.V15.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.MokhamedAA.Sprint3.Task5.V15.Lib;
+namespace Tyuiu.MokhamedAA.Sprint3.Task5.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MokhamedAA.Sprint3.Task5.V15/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.MokhamedAA.Sprint3.Task5.V15.Lib;
+namespace Tyuiu.MokhamedAA.Sprint3.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Переменная X: {x}");
+            Console.WriteLine($"Старт шага первой суммы ряда: {startValue1}");
+            Console.WriteLine($"Конец шага первой суммы ряда: {startValue2}");
+            Console.WriteLine($"Старт шага второй суммы ряда: {stopValue1}");
+            Console.WriteLine($"Конец шага второй суммы ряда: {stopValue2}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write($"Результат = {ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2)}");
+            Console.ReadKey();
+        }
+    }
+}
